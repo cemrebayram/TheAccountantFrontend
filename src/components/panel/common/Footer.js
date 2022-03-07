e70b0c1f500d21f7e94ce6d2a74a335d3326a8d3
cemrebayram <60187245+cemrebayram@users.noreply.github.com>
2022-03-07 20:53:58 +0100

    Update Footer.js

--- src/components/panel/common/Footer.js
+++ src/components/panel/common/Footer.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import {Typography } from '@mui/material'
+import React from "react";
+import { Typography } from "@mui/material";
 
 export default function Footer() {
-    return (
+  return (
     <footer>
-        <Typography variant="overline">
-            The Accountant &copy; 2022 All Rights Reserved
-        </Typography>
+      <Typography variant="overline">
+        The Accountant &copy; 2022 All Rights Reserved -{" "}
+        <a
+          href="https://www.flaticon.com/de/kostenlose-icons/open-source"
+          title="open source Icons"
+        >
+          Open source Icons erstellt von Pixel perfect - Flaticon
+        </a>
+      </Typography>
     </footer>
   );
 }
- 
