d34f89ee7fe09bdcfc333b63f94060197cb392eb
cemrebayram <60187245+cemrebayram@users.noreply.github.com>
2022-03-10 15:24:54 +0100

    design update

--- src/components/panel/common/Footer.js
+++ src/components/panel/common/Footer.js
@@ -6,12 +6,7 @@ export default function Footer() {
     <footer>
       <Typography variant="overline">
         The Accountant &copy; 2022 All Rights Reserved -
-        <a
-          href="https://www.flaticon.com/de/kostenlose-icons/open-source"
-          title="open source Icons"
-        >
-          Open source Icons erstellt von Pixel perfect - Flaticon
-        </a>
+        
       </Typography>
     </footer>
   );
--- src/views/panel/Offer.js
+++ src/views/panel/Offer.js
@@ -1,42 +1,47 @@
-import React from 'react'
-import OfferInputForm from '../../components/panel/offer/OfferInputForm'
-import { Button, Container, Typography, Divider } from '@mui/material'
-import { useSelector,useDispatch } from 'react-redux'
-import { createOffer, fetchOffers } from '../../store/slices/offerSlice'
-import OffersTable from '../../components/panel/offer/OffersTable'
+import React from "react";
+import OfferInputForm from "../../components/panel/offer/OfferInputForm";
+import { Button, Container, Typography, Divider } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { createOffer, fetchOffers } from "../../store/slices/offerSlice";
+import OffersTable from "../../components/panel/offer/OffersTable";
 export default function Offser() {
-    const newOffer = useSelector(state => state.offer.newOffer)
-    const offers = useSelector(state => state.offer.offers)
-    const dispatch = useDispatch();
-    React.useEffect(() => {
-        dispatch(fetchOffers());
-    }, [])
-    return (
-        <Container>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <Divider sx={{ margin: "30px 0"}}>
-            <img width="50px" src="/../../offer.png"></img>
-            </Divider>  
-            <Typography variant="h6">Create Offer</Typography>
-            <OfferInputForm/>
-            <br/>
-            <br/>
-            <Button onClick={()=>{
-                dispatch(createOffer(newOffer));
-                dispatch(fetchOffers());
-            }} sx={{marginTop:'10px'}} fullWidth variant="contained" color="secondary">Create Offer</Button>
-            <OffersTable offers={offers} />
+  const newOffer = useSelector((state) => state.offer.newOffer);
+  const offers = useSelector((state) => state.offer.offers);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(fetchOffers());
+  }, []);
+  return (
+    <Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Divider sx={{ margin: "30px 0" }}>
+        <img width="50px" src="/../../offer.png"></img>
+      </Divider>
+      <Typography variant="h6">Create Offer</Typography>
+      <OfferInputForm />
+      <br />
+      <br />
+      <Button
+        onClick={() => {
+          dispatch(createOffer(newOffer));
+          dispatch(fetchOffers());
+        }}
+        sx={{ marginTop: "10px" }}
+        fullWidth
+        variant="contained"
+        color="secondary"
+      >
+        Create Offer
+      </Button>
+      <OffersTable offers={offers} />
 
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-
-        </Container>
-    )
-
-    
+      <br />
+      <br />
+      <br />
+      <br />
+    </Container>
+  );
 }
