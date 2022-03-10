5af3184bfa768dd9c0aaf2262a978b12becd751d
cemrebayram <60187245+cemrebayram@users.noreply.github.com>
2022-03-10 23:10:21 +0100

    Update Invoice.js

--- src/views/panel/Invoice.js
+++ src/views/panel/Invoice.js
@@ -1,25 +1,40 @@
-import React from 'react'
-import InvoiceInputForm from '../../components/panel/invoice/InvoiceInputForm'
-import { Button, Container, Typography } from '@mui/material'
-import { useSelector,useDispatch } from 'react-redux'
-import { createInvoice, fetchInvoices } from '../../store/slices/invoiceSlice'
-import InvoicesTable from '../../components/panel/invoice/InvoicesTable'
+import React from "react";
+import InvoiceInputForm from "../../components/panel/invoice/InvoiceInputForm";
+import { Button, Container, Typography } from "@mui/material";
+import { useSelector, useDispatch } from "react-redux";
+import { createInvoice, fetchInvoices } from "../../store/slices/invoiceSlice";
+import InvoicesTable from "../../components/panel/invoice/InvoicesTable";
 export default function Offser() {
-    const newInvoice = useSelector(state => state.invoice.newInvoice)
-    const invoices = useSelector(state => state.invoice.invoices)
-    const dispatch = useDispatch();
-    React.useEffect(() => {
-        dispatch(fetchInvoices());
-    }, [])
-    return (
-        <Container>
-            <Typography variant="h4">Invoice</Typography>
-            <InvoiceInputForm/>
-            <Button onClick={()=>{
-                dispatch(createInvoice(newInvoice));
-                dispatch(fetchInvoices());
-            }} sx={{marginTop:'10px'}} fullWidth variant="contained" color="warning">Create Invoice</Button>
-            <InvoicesTable invoices={invoices} />
-        </Container>
-    )
+  const newInvoice = useSelector((state) => state.invoice.newInvoice);
+  const invoices = useSelector((state) => state.invoice.invoices);
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    dispatch(fetchInvoices());
+  }, []);
+  return (
+    <Container>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Divider sx={{ margin: "30px 0" }}>
+        <img width="50px" src="/../../offer.png"></img>
+      </Divider>
+      <Typography variant="h4">Invoice</Typography>
+      <InvoiceInputForm />
+      <Button
+        onClick={() => {
+          dispatch(createInvoice(newInvoice));
+          dispatch(fetchInvoices());
+        }}
+        sx={{ marginTop: "10px" }}
+        fullWidth
+        variant="contained"
+        color="secondary"
+      >
+        Create Invoice
+      </Button>
+      <InvoicesTable invoices={invoices} />
+    </Container>
+  );
 }
