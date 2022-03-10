c7382e2ac0fcfdec2368e23ac5d1a1c05cddc432
cemrebayram <60187245+cemrebayram@users.noreply.github.com>
2022-03-10 20:01:24 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './style.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./style.css";
 import { BrowserRouter } from "react-router-dom";
-import store from './store/store';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
-import { Provider } from 'react-redux'
+import store from "./store/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { Provider } from "react-redux";
 let persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
@@ -16,5 +17,5 @@ ReactDOM.render(
       </BrowserRouter>
     </PersistGate>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
